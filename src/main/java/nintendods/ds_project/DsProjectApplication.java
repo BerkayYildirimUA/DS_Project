b1c6c2a7f9c27d877a853d8b76c6ce8f91229cee
BerkayYildirimUA <Berkay.Yildirim@student.uantwerpen.be>
2024-03-11 10:30:55 +0100

    NameServerDatabase toegevoed

--- src/main/java/nintendods/ds_project/DsProjectApplication.java
+++ src/main/java/nintendods/ds_project/DsProjectApplication.java
@@ -3,11 +3,13 @@ package nintendods.ds_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class DsProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DsProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DsProjectApplication.class, args);
+    }
 
 }
