4405cf51487a1878162f466642037f8f102f3e6a
BerkayYildirimUA <Berkay.Yildirim@student.uantwerpen.be>
2024-03-23 17:32:13 +0100

    Revert "Merge pull request #8 from BerkayYildirimUA/fixTomMerge"
    This reverts commit 38c3bff828e2b2f004944ed21bf24c37e290d9a9, reversing
    changes made to f5851ab48346b3ec981da2df7cc57bdb840b92f0.
    
    revert

--- Client/src/main/java/nintendods/ds_project/DsProjectApplication.java
+++ Client/src/main/java/nintendods/ds_project/DsProjectApplication.java
@@ -3,18 +3,12 @@ package nintendods.ds_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@RestController
 public class DsProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(DsProjectApplication.class, args);
     }
-
-    @GetMapping("/")
-    public String check() { return "Project is running"; }
 }
--- NameServer/src/main/java/nintendods/ds_project/utility/ResolveFileId.java
+++
@@ -1,4 +0,0 @@
-package nintendods.ds_project.utility;
-
-public class ResolveFileId {
-}
