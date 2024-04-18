6fe3f23504d5d11caa0b16c4740870ba158815c4
Robbe Elsermans <robels@outlook.com>
2024-04-18 09:53:02 +0200

    wrong constr

--- Client/src/main/java/nintendods/ds_project/model/message/UNAMObject.java
+++ Client/src/main/java/nintendods/ds_project/model/message/UNAMObject.java
@@ -13,7 +13,7 @@ public class UNAMObject extends AMessage {
      * @param type The type of unicast message defined in {@link eMessageTypes}
      * @param amountOfNodes The amount of nodes present in the network (exclusive)
      */
-    public UNAMObject(long messageId, eMessageTypes type, int amountOfNodes) {
+    public UNAMObject(long messageId, eMessageTypes type, int amountOfNodes, String address) {
         super(messageId, type);
         setAmountOfNodes(amountOfNodes);
         setNSAddress(address);
