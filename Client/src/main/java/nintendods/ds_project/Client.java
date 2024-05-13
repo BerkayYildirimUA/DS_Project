3eddf3fa333f579fa5e2d70c99799f24c3fba5df
BerkayYildirimUA <Berkay.Yildirim@student.uantwerpen.be>
2024-05-13 10:21:00 +0200

    started adding replication shutdown.

--- Client/src/main/java/nintendods/ds_project/Client.java
+++ Client/src/main/java/nintendods/ds_project/Client.java
@@ -9,10 +9,7 @@ import nintendods.ds_project.exeption.DuplicateNodeException;
 import nintendods.ds_project.exeption.NotEnoughMessageException;
 import nintendods.ds_project.model.ClientNode;
 import nintendods.ds_project.model.message.UNAMObject;
-import nintendods.ds_project.service.DiscoveryService;
-import nintendods.ds_project.service.MulticastListenerService;
-import nintendods.ds_project.service.NSAPIService;
-import nintendods.ds_project.service.UnicastListenerService;
+import nintendods.ds_project.service.*;
 import nintendods.ds_project.utility.JsonConverter;
 import nintendods.ds_project.utility.Generator;
 import org.hibernate.annotations.Synchronize;
@@ -114,7 +111,14 @@ public class Client {
     public void prepareForShutdown() throws InterruptedException {
         if (nodeState != eNodeState.DISCOVERY) {
             System.out.println("Preparing for shutdown...");
-            shutdown();
+            ShutdownService shutdownService;
+            if (testing == 1){
+                shutdownService = new ShutdownService(node, nsObject, t_nextNodePort, t_prevNodePort);
+            } else {
+                shutdownService = new ShutdownService(node, nsObject);
+            }
+
+            shutdownService.updateNodesInSystem();
             System.out.println("Nodes prepared.");
             isRunning = false;
             System.out.println("PreDestroy done");
@@ -273,59 +277,4 @@ public class Client {
 
         System.out.println("Main Done");
     }
-
-
-    private void shutdown() {
-        RestTemplate restTemplate = new RestTemplate();
-
-        //if you are alone in the network then skip
-        if (node.getId() != node.getPrevNodeId()) {
-            int nextNodePort;
-            int prevNodePort;
-
-            String prevNodeIP;
-            String nextNodeIP;
-
-            //multiple springboot appliactions can't be run on the same port on the same machine. So this is made, so I can change the Port numbers dynamicly during testing.
-            if (testing == 1) {
-                nextNodePort = t_nextNodePort;
-                prevNodePort = t_prevNodePort;
-
-                prevNodeIP = "/127.0.0.1";
-                nextNodeIP = "/127.0.0.1";
-            } else {
-                nextNodePort = apiPort;
-                prevNodePort = apiPort;
-
-
-                String urlGetNextNodeIP = "http://" + nsObject.getNSAddress() + ":8089/node/" + node.getNextNodeId();
-                logger.info("GET from: " + urlGetNextNodeIP);
-                ResponseEntity<String> getNextNodeIDResponse = restTemplate.getForEntity(urlGetNextNodeIP, String.class);
-                nextNodeIP = getNextNodeIDResponse.getBody();
-
-                String urlGetPrevNodeIP = "http://" + nsObject.getNSAddress() + ":8089/node/" + node.getPrevNodeId();
-                logger.info("GET from: " + urlGetPrevNodeIP);
-                ResponseEntity<String> getPrevNodeIDResponse = restTemplate.getForEntity(urlGetPrevNodeIP, String.class);
-                prevNodeIP = getPrevNodeIDResponse.getBody();
-
-            }
-
-            // in PREV node I need to change their NEXT node to my NEXT node
-            // http heeft 1 '/' in de plaats van 2 want the IP's strings starten met '/' en ik will dit niet uit filtreren.
-            String UrlForPrevNode = "http:/" + prevNodeIP + ":" + prevNodePort + "/api/Management/nextNodeID/?ID=" + node.getNextNodeId();
-            logger.info("PUT to: " + UrlForPrevNode);
-            restTemplate.put(UrlForPrevNode, String.class);
-
-            // in NEXT node I need to change their PREV node to my PREV node
-            String urlForNextNode = "http:/" + nextNodeIP + ":" + nextNodePort + "/api/Management/prevNodeID/?ID=" + node.getPrevNodeId();
-            logger.info("PUT to: " + urlForNextNode);
-            restTemplate.put(urlForNextNode, String.class);
-        }
-        //delete yourself from nameserver
-        String urlDeleteNode = "http://" + nsObject.getNSAddress() + ":8089/nodes/" + node.getId();
-        logger.info("DELETE from: " + urlDeleteNode);
-        restTemplate.delete(urlDeleteNode);
-    }
-
-
 }
